45b876064888a79d3a3f4da34b07c2df78c7f99f
patnir <rahul20patni@gmail.com>
2017-01-21 15:30:24 -0500

    choosing different buttons

--- app/src/main/java/com/example/rahulpatni/heromake/Activities/MainActivity.java
+++ app/src/main/java/com/example/rahulpatni/heromake/Activities/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.rahulpatni.heromake.Activities;
 
+import android.net.Uri;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.rahulpatni.heromake.Fragments.MainFragment;
 import com.example.rahulpatni.heromake.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainFragment.MainFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager manager = getSupportFragmentManager();
+
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new MainFragment();
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.add(R.id.fragment_container, fragment);
+            transaction.commit();
+
+        }
+    }
+
+    @Override
+    public void onMainFragmentInteraction(Uri uri) {
     }
 }
--- app/src/main/java/com/example/rahulpatni/heromake/Fragments/MainFragment.java
+++ app/src/main/java/com/example/rahulpatni/heromake/Fragments/MainFragment.java
@@ -7,18 +7,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.rahulpatni.heromake.R;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link MainFragment.OnFragmentInteractionListener} interface
+ * {@link MainFragment.MainFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link MainFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements View.OnClickListener{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -28,7 +29,12 @@ public class MainFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    private Button btnAccident;
+    private Button btnBorn;
+    private Button btnGenetic;
+    private Button btnChoose;
+
+    private MainFragmentInteractionListener mListener;
 
     public MainFragment() {
         // Required empty public constructor
@@ -53,6 +59,35 @@ public class MainFragment extends Fragment {
     }
 
     @Override
+    public void onClick(View v) {
+        btnChoose.setEnabled(true);
+        btnChoose.getBackground().setAlpha(255);
+
+
+        Button button = (Button) v;
+
+        btnAccident.setCompoundDrawablesWithIntrinsicBounds(R.drawable.lightning_icon, 0, 0, 0);
+        btnGenetic.setCompoundDrawablesWithIntrinsicBounds(R.drawable.atomic_icon, 0, 0, 0);
+        btnBorn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.rocket_icon, 0, 0, 0);
+
+        int leftDrawable = 0;
+
+        if (button == btnAccident) {
+            leftDrawable = R.drawable.lightning_icon;
+        }
+        else if (button == btnBorn) {
+            leftDrawable = R.drawable.atomic_icon;
+
+        } else {
+            leftDrawable = R.drawable.rocket_icon;
+        }
+
+        button.setCompoundDrawablesWithIntrinsicBounds(leftDrawable,0,R.drawable.item_selected_btn, 0);
+
+
+    }
+
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
@@ -65,21 +100,35 @@ public class MainFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        btnAccident = (Button) view.findViewById(R.id.btnAccident);
+        btnBorn = (Button) view.findViewById(R.id.btnBorn);
+        btnChoose = (Button) view.findViewById(R.id.btnChoose);
+        btnGenetic = (Button) view.findViewById(R.id.btnGenetic);
+
+        btnAccident.setOnClickListener(this);
+        btnBorn.setOnClickListener(this);
+        //btnChoose.setOnClickListener(this);
+        btnGenetic.setOnClickListener(this);
+
+
+        btnChoose.setEnabled(false);
+        btnChoose.getBackground().setAlpha(128);
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+            mListener.onMainFragmentInteraction(uri);
         }
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        if (context instanceof MainFragmentInteractionListener) {
+            mListener = (MainFragmentInteractionListener) context;
         } else {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
@@ -102,8 +151,8 @@ public class MainFragment extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
+    public interface MainFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        void onMainFragmentInteraction(Uri uri);
     }
 }
